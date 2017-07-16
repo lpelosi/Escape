4098a73bd3f1a5ec659bffc7410ca2d2778182b4
lpelosi <lrpelosi@gmail.com>
2017-07-16 16:30:21 -0400

    Created objects Door and Chair and set door to movable 60°

--- Source/Escape/OpenDoor.cpp
+++ Source/Escape/OpenDoor.cpp
@@ -1,12 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Escape.h"
-#include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,23 +16,22 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
     
-    FString ObjectName = GetOwner()->GetName();
-    FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-    
-    
-    UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
-
-	// ...
+    // Get actor owner
+    AActor* Owner = GetOwner();
+    // Create Rotator
+    FRotator NewRotation = FRotator(0.0f, 60.0f, 0.0f);
+	// Set the door rotation
+    Owner->SetActorRotation(NewRotation);
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
